f560fb54cdc0e9d40cf87787ee027cd11505aa93
GokoshiJr <julioagonzalez18@gmail.com>
2020-11-22 18:40:00 -0400

    Ruta y vista autores, navbar

--- src/routes/authors.js
+++ src/routes/authors.js
@@ -3,6 +3,13 @@ const router = express.Router();
 // pool = conexi db
 const pool = require("../database");
 
+router.get("/add", (req, res) => {
+  res.render("authors/add"); // archivo con la vista .hbs
+});
 
+router.post("/add", (req, res) => {
+  const { name, nacionality } = req.body;
+  pool.query("INSERT INTO authors (name, nacionality) VALUES (?, ?)", [name, nacionality]);
+});
 
 module.exports = router;
